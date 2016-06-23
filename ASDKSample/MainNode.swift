4ab9c5805c29042565a3fb436d882a7df452dc25
Bartosz Górka <bartosz.gorka@droidsonroids.pl>
2016-06-23 23:37:04 +0200

    simple layout spec

--- ASDKSample/MainNode.swift
+++ ASDKSample/MainNode.swift
@@ -11,9 +11,33 @@ import AsyncDisplayKit
 
 class MainNode: ASDisplayNode {
     
+    var firstNode: ASDisplayNode
+    var secondNode: ASDisplayNode
+    
+    override init() {
+        firstNode = ASDisplayNode()
+        secondNode = ASDisplayNode()
+        
+        super.init()
+        
+        addSubnode(firstNode)
+        addSubnode(secondNode)
+    }
+    
+    override func layoutSpecThatFits(constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        firstNode.preferredFrameSize = CGSize(width: 50.0, height: 50.0)
+        secondNode.preferredFrameSize = CGSize(width: 50.0, height: 50.0)
+        
+        let mainSpec = ASStackLayoutSpec(direction: .Horizontal, spacing: 0.0, justifyContent: .Center, alignItems: .Center, children: [firstNode, secondNode])
+        
+        return mainSpec
+    }
+    
     override func didLoad() {
         super.didLoad()
         
-        backgroundColor = .grayColor()
+        backgroundColor = .whiteColor()
+        firstNode.backgroundColor = .grayColor()
+        secondNode.backgroundColor = .lightGrayColor()
     }
 }
